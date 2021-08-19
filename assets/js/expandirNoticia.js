49bb3a862fe52ab7c7ca712866eca1ebe264b185
marcosdsv <marcosbmx120@hotmail.com>
2021-08-19 08:34:38 -0300

    adicionei código java script para expandir as notícias

--- assets/js/expandirNoticia.js
+++ assets/js/expandirNoticia.js
@@ -6,3 +6,7 @@ function mostrarNoticia2(){
 	document.getElementById("corpoNoticia2").style.display = 'block';
 	document.getElementById("link2").style.display = 'none';
 }
+function mostrarNoticia3(){
+	document.getElementById("corpoNoticia3").style.display = 'block';
+	document.getElementById("link3").style.display = 'none';
+}
