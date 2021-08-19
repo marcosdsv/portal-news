b128f841c21d97af8e2f2ed518de4edf43f32d1d
marcosdsv <marcosbmx120@hotmail.com>
2021-08-19 06:59:50 -0300

    adicionei a pasta assets e suas subpastas img, js, css

---
+++ assets/js/expandirNoticia.js
@@ -0,0 +1,8 @@
+function mostrarNoticia1(){
+	document.getElementById("corpoNoticia1").style.display = 'block';
+	document.getElementById("link1").style.display = 'none';
+}
+function mostrarNoticia2(){
+	document.getElementById("corpoNoticia2").style.display = 'block';
+	document.getElementById("link2").style.display = 'none';
+}
---
+++ assets/js/fecharNoticia.js
@@ -0,0 +1,5 @@
+function fechar1(){
+	document.getElementById("link1").style.display = 'block';
+	document.getElementById("link1").focus();
+	document.getElementById("corpoNoticia1").style.display = 'none';
+}
