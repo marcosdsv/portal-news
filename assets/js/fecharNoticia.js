589db41a7c86bc540217dcf872b1ad991c305094
marcosdsv <marcosbmx120@hotmail.com>
2021-08-19 08:36:14 -0300

    adicionei código java script para fechar as notícias

--- assets/js/fecharNoticia.js
+++ assets/js/fecharNoticia.js
@@ -1,5 +1,15 @@
 function fechar1(){
 	document.getElementById("link1").style.display = 'block';
-	document.getElementById("link1").focus();
 	document.getElementById("corpoNoticia1").style.display = 'none';
+	document.getElementById("link1").focus();
+}
+function fechar2(){
+	document.getElementById("link2").style.display = 'block';
+document.getElementById("corpoNoticia2").style.display = 'none';
+document.getElementById("link2").focus();
+}
+function fechar3(){
+	document.getElementById("link3").style.display = 'block';
+document.getElementById("corpoNoticia3").style.display = 'none';
+document.getElementById("link3").focus();
 }
