ca689540c5173d1886830cfaa5069099851f853d
marcosdsv <marcosbmx120@hotmail.com>
2021-08-19 17:46:37 -0300

    modifiquei a função espandir notícia

--- assets/js/expandirNoticia.js
+++ assets/js/expandirNoticia.js
@@ -10,3 +10,15 @@ function mostrarNoticia3(){
 	document.getElementById("corpoNoticia3").style.display = 'block';
 	document.getElementById("link3").style.display = 'none';
 }
+function mostrarNoticia4(){
+	document.getElementById("corpoNoticia4").style.display = 'block';
+	document.getElementById("link4").style.display = 'none';
+}
+function mostrarNoticia5(){
+	document.getElementById("corpoNoticia5").style.display = 'block';
+	document.getElementById("link5").style.display = 'none';
+}
+function mostrarNoticia6(){
+	document.getElementById("corpoNoticia6").style.display = 'block';
+	document.getElementById("link6").style.display = 'none';
+}
