241209a48bb8399ed8906dc2b59d21982f79c2ac
marcosdsv <marcosbmx120@hotmail.com>
2021-08-21 06:38:40 -0300

    criei o arquivo validaForm.js

---
+++ assets/js/validaForm.js
@@ -0,0 +1,12 @@
+function validaCampos(){
+	var nome = document.getElementById("nomeusuario").value;
+	var email = document.getElementById("email").value;
+	var assunto = document.getElementById("assunto").value;
+		var msg = document.getElementById("msg").value;
+if(nome != "" && email != "" && assunto != "" && msg != ""){
+	alert("Sua mensagem foi enviada com sucesso");
+}else{
+	alert("todos os campos são obrigatório");
+}
+	
+}
