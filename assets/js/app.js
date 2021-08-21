b769dd299b63cd592420ed9c9ff666a906ee1c43
Raphael dos Anjos <raphaanjos1@gmail.com>
2021-08-21 07:12:47 -0300

    Aplicando o CSS no arquivo index.html. Adicionei o required nos inputs do formulário contato, em contato.html

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 function toggleSide() {
     let x = document.getElementById('sidePanel');
     (x.style.left == undefined || x.style.left === "-460px" || x.style.left === "") ?
-     x.style.left = "0px" : x.style.left = "-460px";
+    x.style.left = "0px" : x.style.left = "-460px";
 }
 
 function closeSide() {
     document.getElementById("sidePanel").style.left = "-460px";
-  }
+}
