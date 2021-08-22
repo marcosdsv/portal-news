331c1ec2abba83afd80c72a654f8b3d6ab4bc53c
Raphael dos Anjos <raphaanjos1@gmail.com>
2021-08-22 01:48:22 -0300

    Atualizando a página saibamais.

--- assets/js/validaForm.js
+++ assets/js/validaForm.js
@@ -3,10 +3,9 @@ function validaCampos(){
 	var email = document.getElementById("email").value;
 	var assunto = document.getElementById("assunto").value;
 		var msg = document.getElementById("msg").value;
-if(nome != "" && email != "" && assunto != "" && msg != ""){
-	alert("Sua mensagem foi enviada com sucesso");
-}else{
-	alert("todos os campos são obrigatório");
-}
-	
+	if(nome != "" && email != "" && assunto != "" && msg != ""){
+		alert("Sua mensagem foi enviada com sucesso");
+	}else{
+		alert("todos os campos são obrigatório");
+	}
 }
