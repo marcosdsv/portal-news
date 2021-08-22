85c2c8b2e73e6916591356261065e6577bbf71da
Raphael dos Anjos <raphaanjos1@gmail.com>
2021-08-22 19:02:44 -0300

    Realizei alterações no tamanho da foto raphael.jpg, no arquivo app.js. Removi o menu lateral (oculto). Ajustei o menu horizontal para ficar responsivo.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,4 @@
-function toggleSide() {
-    let x = document.getElementById('sidePanel');
-    (x.style.left == undefined || x.style.left === "-460px" || x.style.left === "") ?
-    x.style.left = "0px" : x.style.left = "-460px";
-}
-
-function closeSide() {
-    document.getElementById("sidePanel").style.left = "-460px";
+function toggleMenu() {
+    let x = document.getElementById('navbar');
+    (x.className === "navbar") ? x.className += " nav-block" : x.className = "navbar";
 }
