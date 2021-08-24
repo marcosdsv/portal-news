71ecaa296887529776d77114a512041e4baf9c06
marcosdsv <marcosbmx120@hotmail.com>
2021-08-24 14:17:38 -0300

    troquei o nome do id do textarea que estava trocado

--- assets/js/validaForm.js
+++ assets/js/validaForm.js
@@ -3,7 +3,7 @@ function validaCampos(){
 	var email = document.getElementById("email").value;
 	var assunto = document.getElementById("assunto").value;
 		var msg = document.getElementById("descricao").value;
-	if(nome != "" && email != "" && assunto != "" && descricao != ""){
+	if(nome != "" && email != "" && assunto != "" && msg != ""){
 		alert("Sua mensagem foi enviada com sucesso");
 	}else{
 		alert("todos os campos são obrigatórios");
